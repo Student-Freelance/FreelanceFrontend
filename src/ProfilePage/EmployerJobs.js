3c52e8a35964bd39010c5496c8fa61efff55d7ac
Oliver <oliverskoeppen@gmail.com>
2019-10-24 11:24:56 +0200

    more stateless components for employercontainer

---
+++ src/ProfilePage/EmployerJobs.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function EmployerJobs(props) {
+    const jobs = props.jobs;
+
+    return(
+        jobs.map(job =>
+            <li key={job.id}>
+                {job.name}
+            </li>
+        )
+    )
+}
+
+export default EmployerJobs;
--- src/ProfilePage/EmployerPage.js
+++ src/ProfilePage/EmployerPage.js
@@ -1,12 +1,26 @@
 import React, {Component} from "react";
 import Button from "react-bootstrap/Button";
+import EmployerJobs from "./EmployerJobs";
 
 class EmployerPage extends Component {
     constructor(props) {
         super(props);
+
+        //TODO: skal hentes fra backend (bortset fra editMode)
         this.state = {
             editMode: false,
-        }
+            firm: {
+                name: "placeholder name",
+                description: "placeholder desc",
+                logo: "https://onlinesolutiongroup.dk/wp-content/uploads/2016/07/IBM-logo.png",
+                jobs: [{
+                    id: "alsdjkas",
+                    name: "Full-stack i Lyngby"
+                }]
+            }
+        };
+
+        console.table(this.state);
     }
 
     //Changes state if user wants to edit
@@ -14,15 +28,38 @@ class EmployerPage extends Component {
         this.setState(state => ({editMode: !state.editMode}))
     };
 
+    handleInputChange(e) {
+       const firmChange = {
+           ...this.state.firm
+       };
+
+       firmChange.description = e.target.value;
+
+       this.setState({firm: firmChange});
+    }
+
     render() {
-        return(
+        return (
             <div>
-                <p>{this.props.firmName}</p>
-                {/*<img src={this.props.logo ||null} alt="logo"/>*/}
-                <p>{this.props.description}</p>
+                <img src={this.state.firm.logo || null} alt="logo" style={{height: "100px"}}/>
+                <h1>{this.state.firm.name}</h1>
                 {this.state.editMode ?
-                    <Button color="success">Save</Button> :
-                    <Button color="primary" onClick={this.toggleEdit()}>Edit</Button>}
+                    <div>
+
+                        <Button color="success"
+                                onClick={() => this.toggleEdit()}>Save</Button>
+                        <input type="text"
+                               value={this.state.firm.description}
+                               onChange={e => this.handleInputChange(e)}/>
+                    </div>
+                    :
+                    <div>
+                        <Button color="primary"
+                                onClick={() => this.toggleEdit()}>Edit</Button>
+                        <p>{this.state.firm.description}</p>
+                    </div>
+                }
+                    <EmployerJobs jobs={this.state.firm.jobs}/>
             </div>
         );
     }

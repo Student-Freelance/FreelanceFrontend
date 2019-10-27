c8a61d00f2c7a73ef85e53e3fb38746536f919f2
AM <lgv2800@gmail.com>
2019-10-27 15:36:48 +0100

    Added padding and buttons in EmployerLoginTab.

--- src/Login/LoginTabs/EmployerLoginTab/EmployerLoginTab.js
+++ src/Login/LoginTabs/EmployerLoginTab/EmployerLoginTab.js
@@ -1,13 +1,20 @@
 import React from "react";
 import './EmployerLoginTab.css';
-import {Tab, Tabs} from 'react-bootstrap';
+import {Tab, Tabs, Button} from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 
 function EmployerLoginTab() {
     return (
 
         <div>
-            <p>Employer</p>
+            <Container className="LoginButtons">
+                <Button variant="primary" size="lg" block>
+                    Employer button 1
+                </Button>
+                <Button variant="secondary" size="lg" block>
+                    Employer button 2
+                </Button>
+            </Container>
         </div>
 
     )

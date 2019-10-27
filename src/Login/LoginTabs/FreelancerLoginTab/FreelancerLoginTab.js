f4a80331c333bfe6bfe305359ee7c8f7eee8872f
AM <lgv2800@gmail.com>
2019-10-27 15:06:32 +0100

    Added folder LoginTabs and subfolder FreelancerLoginTab with corrosponding js file.

---
+++ src/Login/LoginTabs/FreelancerLoginTab/FreelancerLoginTab.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Tab, Tabs} from 'react-bootstrap';
+import Container from "react-bootstrap/Container";
+
+function FreelancerLoginTab() {
+    return (
+
+        <div>
+            <p>Freelancer</p>
+        </div>
+
+    )
+}
+
+export default FreelancerLoginTab;

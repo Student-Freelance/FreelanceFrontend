e83a949114f86cb04be2a8c1de660913f1dca961
AM <lgv2800@gmail.com>
2019-10-27 15:46:17 +0100

    Added login with mail old school option form in EmployerLoginTab.

--- src/Login/LoginTabs/EmployerLoginTab/EmployerLoginTab.js
+++ src/Login/LoginTabs/EmployerLoginTab/EmployerLoginTab.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './EmployerLoginTab.css';
-import {Tab, Tabs, Button} from 'react-bootstrap';
+import {Tab, Tabs, Button, Form, Dropdown} from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 
 function EmployerLoginTab() {
@@ -14,6 +14,31 @@ function EmployerLoginTab() {
                 <Button variant="secondary" size="lg" block>
                     Employer button 2
                 </Button>
+            </Container>
+            <Container className="LoginDivider">
+                <Dropdown.Divider />
+            </Container>
+            <Container>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Remember me?" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" size="lg" block>
+                        Submit
+                    </Button>
+                </Form>
             </Container>
         </div>
 

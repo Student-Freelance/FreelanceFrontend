5dd5c6386fae3e77807a98d55b240e26fe47b6ec
AM <lgv2800@gmail.com>
2019-10-27 17:28:24 +0100

    Changed design in EmployerLoginTab.

--- src/Login/LoginTabs/EmployerLoginTab/EmployerLoginTab.js
+++ src/Login/LoginTabs/EmployerLoginTab/EmployerLoginTab.js
@@ -1,47 +1,52 @@
 import React from "react";
 import './EmployerLoginTab.css';
-import {Tab, Tabs, Button, Form, Dropdown} from 'react-bootstrap';
+import {Tab, Tabs, Button, Form, Dropdown, Card} from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 
 function EmployerLoginTab() {
     return (
-
         <div>
-            <Container className="LoginButtons">
-                <Button variant="dark" size="lg" block>
-                    Employer button 1
-                </Button>
-                <Button variant="dark" size="lg" block>
-                    Employer button 2
-                </Button>
-                <Button variant="dark" size="lg" block>
-                    Employer button 3
-                </Button>
-            </Container>
-            <Container className="LoginDivider">
-                <Dropdown.Divider />
-            </Container>
-            <Container>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+            <Container className="LoginForm">
+                <Card>
+                    <Card.Header as="h5">Sign in with mail</Card.Header>
+                    <Card.Body>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Control type="email" placeholder="Enter email" />
+                            </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Remember me?" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" size="lg" block>
-                        Submit
-                    </Button>
-                </Form>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Remember me?" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit" size="lg" block>
+                                Sign in
+                            </Button>
+                            <Button variant="secondary" type="submit" size="lg" block>
+                                Sign up
+                            </Button>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Container>
+            <Container className="LoginDivider"></Container>
+            <Container className="LoginButtons">
+                <Card>
+                    <Card.Header as="h5">Sign in with third parties</Card.Header>
+                    <Card.Body>
+                        <Button variant="dark" size="lg" block>
+                            Freelancer button 1
+                        </Button>
+                        <Button variant="dark" size="lg" block>
+                            Freelancer button 2
+                        </Button>
+                        <Button variant="dark" size="lg" block>
+                            Freelancer button 3
+                        </Button>
+                    </Card.Body>
+                </Card>
             </Container>
         </div>
 

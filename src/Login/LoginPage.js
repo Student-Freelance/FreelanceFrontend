09d354f8b1687b3a87f1ba70f76ec77d646a284d
AM <lgv2800@gmail.com>
2019-10-27 15:02:23 +0100

    Added basic Tabs bar in LoginPage. Removed old content.

--- src/Login/LoginPage.js
+++ src/Login/LoginPage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import {Tab, Tabs} from 'react-bootstrap';
+import Container from "react-bootstrap/Container";
 
 function LoginPage() {
     return (
         <div>
-            <h1>Login page!</h1>
+            <Container className="LoginCard">
+                <Tabs defaultActiveKey="loginFreelancer" id="uncontrolled-tab-example">
+                    <Tab eventKey="loginFreelancer" title="Freelancer">
+                        {/* <Sonnet /> */}
+                    </Tab>
+                    <Tab eventKey="loginEmployer" title="Employer">
+                        {/* <Sonnet /> */}
+                    </Tab>
+                </Tabs>
+            </Container>
         </div>
     )
 }

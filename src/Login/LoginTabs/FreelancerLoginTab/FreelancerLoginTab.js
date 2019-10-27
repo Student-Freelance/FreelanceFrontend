2b0a5ab32e6ddb21aaeaf69533c95171a56bd253
AM <lgv2800@gmail.com>
2019-10-27 15:20:39 +0100

    Added FreelanceLoginTab css file and it's import statement in js file.

--- src/Login/LoginTabs/FreelancerLoginTab/FreelancerLoginTab.js
+++ src/Login/LoginTabs/FreelancerLoginTab/FreelancerLoginTab.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Tab, Tabs} from 'react-bootstrap';
+import './FreelancerLoginTab.css';
 import Container from "react-bootstrap/Container";
 
 function FreelancerLoginTab() {

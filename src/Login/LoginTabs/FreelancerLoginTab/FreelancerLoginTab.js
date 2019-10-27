072c42fe093c427424714ffa90cf0474fb6831ac
AM <lgv2800@gmail.com>
2019-10-27 17:28:16 +0100

    Changed design in FreelancerLoginTab.

--- src/Login/LoginTabs/FreelancerLoginTab/FreelancerLoginTab.js
+++ src/Login/LoginTabs/FreelancerLoginTab/FreelancerLoginTab.js
@@ -6,6 +6,29 @@ import Container from "react-bootstrap/Container";
 function FreelancerLoginTab() {
     return (
         <div>
+            <Container className="LoginForm">
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Remember me?" />
+                    </Form.Group>
+                    <Button variant="primary" type="submit" size="lg" block>
+                        Sign in
+                    </Button>
+                    <Button variant="secondary" type="submit" size="lg" block>
+                        Sign up
+                    </Button>
+                </Form>
+            </Container>
+            <Container className="LoginDivider">
+                <Dropdown.Divider />
+            </Container>
             <Container className="LoginButtons">
                 <Button variant="dark" size="lg" block>
                     Freelancer button 1
@@ -16,31 +39,6 @@ function FreelancerLoginTab() {
                 <Button variant="dark" size="lg" block>
                     Freelancer button 3
                 </Button>
-            </Container>
-            <Container className="LoginDivider">
-                <Dropdown.Divider />
-            </Container>
-            <Container>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Remember me?" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" size="lg" block>
-                        Submit
-                    </Button>
-                </Form>
             </Container>
         </div>
     )

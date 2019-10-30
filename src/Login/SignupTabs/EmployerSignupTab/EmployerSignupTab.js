8438c9a0a37386cb534b83b2e6077b6d40036a55
AM <lgv2800@gmail.com>
2019-10-30 21:30:51 +0100

    Added event changes of form values in EmployerSignupTab.

--- src/Login/SignupTabs/EmployerSignupTab/EmployerSignupTab.js
+++ src/Login/SignupTabs/EmployerSignupTab/EmployerSignupTab.js
@@ -1,44 +1,86 @@
-import React from "react";
+import React, {Component} from "react";
 import './EmployerSignupTab.css';
 import {Button, Form, Col, Dropdown, Card} from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 import Axios from "axios";
 
-function EmployerSignupTab() {
-    return (
-        <div>
-            <Container className="LoginForm">
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
+class EmployerSignupTab extends Component {
 
-                    <Form.Group>
-                        <Form.Row>
-                            <Col>
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Enter password" />
-                            </Col>
-                            <Col>
-                                <Form.Label>Confirm password</Form.Label>
-                                <Form.Control type="password" placeholder="Re-enter password" />
-                            </Col>
-                        </Form.Row>
-                    </Form.Group>
+    constructor(props) {
+        super(props);
 
-                    <Form.Group controlId="formCompanyName">
-                        <Form.Label>Company name</Form.Label>
-                        <Form.Control type="text" placeholder="Enter company name" />
-                    </Form.Group>
+        this.state = {
+            email: "",
+            password: "",
+            confirmPassword: "",
+            companyName: ""
+        }
+    }
 
-                    <Button onClick={() => performHTTPRequest()} variant="primary" type="submit" size="lg" block>
-                        Sign up
-                    </Button>
-                </Form>
-            </Container>
-        </div>
-    )
+    handleEmailChange = (event) => {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleConfirmPasswordChange = (event) => {
+        this.setState({
+            confirmPassword: event.target.value
+        })
+    }
+
+    handleCompanyNameChange = (event) => {
+        this.setState({
+            companyName: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Container className="LoginForm">
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" value={this.state.email} placeholder="Enter email" onChange={this.handleEmailChange} />
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Row>
+                                <Col>
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" value={this.state.password} placeholder="Enter password" onChange={this.handlePasswordChange} />
+                                </Col>
+                                <Col>
+                                    <Form.Label>Confirm password</Form.Label>
+                                    <Form.Control type="password" value={this.state.confirmPassword} placeholder="Re-enter password" onChange={this.handleConfirmPasswordChange} />
+                                </Col>
+                            </Form.Row>
+                        </Form.Group>
+
+                        <Form.Group controlId="formCompanyName">
+                            <Form.Label>Company name</Form.Label>
+                            <Form.Control type="text" value={this.state.companyName} placeholder="Enter company name" onChange={this.handleCompanyNameChange} />
+                        </Form.Group>
+
+                        <Button onClick={() => performHTTPRequest()} variant="primary" type="submit" size="lg" block>
+                            Sign up
+                        </Button>
+                    </Form>
+                </Container>
+            </div>
+        );
+    }
 }
 
 async function performHTTPRequest() {

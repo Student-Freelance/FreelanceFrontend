7308d423c98ead0b51cebe0d8f46d5cb125f8eb3
AM <lgv2800@gmail.com>
2019-10-30 20:49:58 +0100

    Changed the structure of file FreelancerLoginTab.

--- src/Login/LoginTabs/FreelancerLoginTab/FreelancerLoginTab.js
+++ src/Login/LoginTabs/FreelancerLoginTab/FreelancerLoginTab.js
@@ -1,48 +1,75 @@
-import React from "react";
+import React, {Component} from "react";
 import './FreelancerLoginTab.css';
 import {Tab, Tabs, Button, Form, Dropdown} from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 import Axios from "axios";
+import EmployerLoginTab from "../EmployerLoginTab/EmployerLoginTab";
 
-function FreelancerLoginTab() {
-    return (
-        <div>
-            <Container className="LoginForm">
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
+class FreelancerLoginTab extends Component {
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Remember me?" />
-                    </Form.Group>
-                    <Button onClick={() => performHTTPRequest()} variant="primary" type="submit" size="lg" block>
-                        Sign in
+    constructor(props) {
+        super(props);
+
+        /*
+        this.state = {
+            showSignupForm: false
+        };*/
+    }
+
+    showSignupFormPage() {
+        //this.setState({ showSignupForm: true});
+
+    }
+
+    render() {
+        /*
+        const signupForm = (
+            <p>HELLO</p>
+        );*/
+
+        const loginForm = (
+            <div>
+                <Container className="LoginForm">
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Remember me?" />
+                        </Form.Group>
+                        <Button onClick={() => performHTTPRequest()} variant="primary" type="submit" size="lg" block>
+                            Sign in
+                        </Button>
+                        <Button onClick={this.showSignupFormPage} variant="secondary" type="submit" size="lg" block>
+                            Sign up
+                        </Button>
+                    </Form>
+                </Container>
+                <Container className="LoginDivider">
+                    <Dropdown.Divider />
+                </Container>
+                <Container className="LoginButtons">
+                    <Button variant="dark" size="lg" block>
+                        Freelancer button 1
                     </Button>
-                    <Button variant="secondary" type="submit" size="lg" block>
-                        Sign up
+                    <Button variant="dark" size="lg" block>
+                        Freelancer button 2
                     </Button>
-                </Form>
-            </Container>
-            <Container className="LoginDivider">
-                <Dropdown.Divider />
-            </Container>
-            <Container className="LoginButtons">
-                <Button variant="dark" size="lg" block>
-                    Freelancer button 1
-                </Button>
-                <Button variant="dark" size="lg" block>
-                    Freelancer button 2
-                </Button>
-                <Button variant="dark" size="lg" block>
-                    Freelancer button 3
-                </Button>
-            </Container>
-        </div>
-    )
+                    <Button variant="dark" size="lg" block>
+                        Freelancer button 3
+                    </Button>
+                </Container>
+            </div>
+        );
+
+        return loginForm
+        /*
+        return { this.state.showSignupForm ? signupForm : loginForm };*/
+    }
 }
 
 async function performHTTPRequest() {

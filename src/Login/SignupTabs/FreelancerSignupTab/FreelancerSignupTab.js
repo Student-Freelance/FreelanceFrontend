9704cd660d686a48f7ebd41a13ee6772e8f20bdf
AM <lgv2800@gmail.com>
2019-10-30 21:12:02 +0100

    Changed FreelancerSignupTab class. Added correct setup for signup.

--- src/Login/SignupTabs/FreelancerSignupTab/FreelancerSignupTab.js
+++ src/Login/SignupTabs/FreelancerSignupTab/FreelancerSignupTab.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import './FreelancerSignupTab.css';
-import {Tab, Tabs, Button, Form, Dropdown} from 'react-bootstrap';
+import {Tab, Tabs, Button, Form, Dropdown, Col} from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 import Axios from "axios";
 
@@ -29,38 +29,47 @@ class FreelancerSignupTab extends Component {
         const loginForm = (
             <div>
                 <Container className="LoginForm">
+                    <Form.Group>
+                        <Form.Row>
+                            <Col>
+                                <Form.Label>First name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter first name" />
+                            </Col>
+                            <Col>
+                                <Form.Label>Last name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter last name" />
+                            </Col>
+                        </Form.Row>
+                    </Form.Group>
+
                     <Form>
                         <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
                             <Form.Control type="email" placeholder="Enter email" />
                         </Form.Group>
 
-                        <Form.Group controlId="formBasicPassword">
-                            <Form.Control type="password" placeholder="Password" />
+                        <Form.Group controlId="formBasicUsername">
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control type="username" placeholder="Enter username" />
                         </Form.Group>
-                        <Form.Group controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Remember me?" />
+
+                        <Form.Group>
+                            <Form.Row>
+                                <Col>
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="Enter password" />
+                                </Col>
+                                <Col>
+                                    <Form.Label>Confirm password</Form.Label>
+                                    <Form.Control type="password" placeholder="Re-enter password" />
+                                </Col>
+                            </Form.Row>
                         </Form.Group>
+
                         <Button onClick={() => performHTTPRequest()} variant="primary" type="submit" size="lg" block>
-                            Sign in
-                        </Button>
-                        <Button onClick={this.showSignupFormPage} variant="secondary" type="submit" size="lg" block>
                             Sign up
                         </Button>
                     </Form>
-                </Container>
-                <Container className="LoginDivider">
-                    <Dropdown.Divider />
-                </Container>
-                <Container className="LoginButtons">
-                    <Button variant="dark" size="lg" block>
-                        Freelancer button 1
-                    </Button>
-                    <Button variant="dark" size="lg" block>
-                        Freelancer button 2
-                    </Button>
-                    <Button variant="dark" size="lg" block>
-                        Freelancer button 3
-                    </Button>
                 </Container>
             </div>
         );

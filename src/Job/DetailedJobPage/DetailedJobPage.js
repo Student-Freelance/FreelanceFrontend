7de16a9896c5f5028a80e1f9f5e1e1ae9f2cd845
AM <lgv2800@gmail.com>
2019-11-13 20:51:36 +0100

    Changed DetailedJobPage. Partially populated page with data from get request.

--- src/Job/DetailedJobPage/DetailedJobPage.js
+++ src/Job/DetailedJobPage/DetailedJobPage.js
@@ -1,6 +1,10 @@
 import React, {Component} from "react";
 import './DetailedJobPage.css';
 import {Badge, Card, Container, Col, Row} from 'react-bootstrap';
+import {AxiosAgent} from "../../Shared/Web/AxiosAgent";
+import Job from "../Job";
+
+const http = new AxiosAgent();
 
 class DetailedJobPage extends Component {
 
@@ -8,148 +12,132 @@ class DetailedJobPage extends Component {
         super(props);
 
         this.state = {
-            jobId: ""
-        }
-
-        console.log(props);
+            job: Job,
+            isLoaded: false
+        };
     }
 
     componentDidMount() {
-        console.log(this.props.match.params);
+        const {handle} = this.props.match.params;
+        try {
+            http.GetOne("Jobs", handle)
+                .then((job) => {
+                    console.table(job);
+                    this.setState({job: job.data});
+                    this.setState({isLoaded: true});
+                    console.log(job)
+                })
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     render() {
-        return (
-            <div>
-                <Container className="summary-Job-Description">
-                    <Container>
-                        <h2>Summary job description (Job title)</h2>
+        return <div>
+            <Container className="summary-Job-Description">
+                <Container>
+                    <h2>{this.state.job.title} - Summary job description (Job title)</h2>
 
-                        <div>
-                            <Badge variant="primary">First tag</Badge>
-                            <Badge variant="primary">Second tag</Badge>
-                            <Badge variant="primary">Third tag</Badge>
-                            <Badge variant="secondary">Fourth tag</Badge>
-                        </div>
+                    {/*<div>
+                        {!this.state.isLoading ? (this.state.job.tags.map(tag => {
+                                return (
+                                    <Badge variant="primary">{tag}</Badge>
+                                );
+                            })
+                        ) : (
+                            <p>Loading...</p>
+                        )}
+                    </div>*/}
 
-                        <Row>
-                            <Col sm={8}>
-                                <Card>
-                                    <Card.Body>
-                                        <p>{this.props.jobId}</p>
-                                        <p>Vi har brug for en frisk person der vil få ansvaret for reaktivering
-                                            tidligere
-                                            kunder. Vi har rigtig gode realtioner og et godt renome hos dem og vi kan
-                                            garanteret nyde godt af at de får lidt fornyet opmærksomhed.</p>
-                                        <br></br>
+                    <div>
+                        <Badge variant="primary">First tag</Badge>
+                        <Badge variant="primary">Second tag</Badge>
+                        <Badge variant="primary">Third tag</Badge>
+                        <Badge variant="secondary">Fourth tag</Badge>
+                    </div>
 
-                                        Dine opgaver vil være at:
-                                        <ul>
-                                            <li>Reaktivere eksisterende kunder hos os (IT-forhandlere) som tidligere har
-                                                handlet, men er stoppet
-                                            </li>
-                                            <li>Få eksisterende mindre kunder til at handler oftere og/ eller mere hos
-                                                os.
-                                            </li>
-                                            <li>Produkterne er computere og printere</li>
-                                            <li>Personen skal være serviceminded, venlig og energisk</li>
-                                            <li>Det er ikke et krav at vedkommende har detaljeret produktviden, men skal
-                                                vide hvad en computer og printer består af overordnet
-                                            </li>
-                                        </ul>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
+                    <Row>
+                        <Col sm={8}>
+                            <Card>
+                                <Card.Body>
+                                    {this.state.job.description}
+                                </Card.Body>
+                            </Card>
+                        </Col>
 
-                            <Col sm={4}>
-                                <Card className="text-center">
-                                    <Card.Body>
-                                        <div className="aboutJobRightCard">
-                                            <Card.Title>Pr. time</Card.Title>
-                                            <Card.Text>DKK 400 - 700.</Card.Text>
-                                        </div>
-                                        <div className="aboutJobRightCard">
-                                            <Card.Title>Job start</Card.Title>
-                                            <Card.Text>Snarest muligt</Card.Text>
-                                        </div>
-                                        <div className="aboutJobRightCard">
-                                            <Card.Title>Arbejdssted</Card.Title>
-                                            <Card.Text>Region: Storkøbenhavn</Card.Text>
-                                            <Card.Text className="text-muted">1456 København K</Card.Text>
-                                        </div>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>
-
+                        <Col sm={4}>
+                            <Card className="text-center">
+                                <Card.Body>
+                                    <div className="aboutJobRightCard">
+                                        <Card.Title>Pr. time</Card.Title>
+                                        <Card.Text>DKK {this.state.job.salary}.</Card.Text>
+                                    </div>
+                                    <div className="aboutJobRightCard">
+                                        <Card.Title>Job start</Card.Title>
+                                        <Card.Text>{this.state.job.jobStart}</Card.Text>
+                                    </div>
+                                    <div className="aboutJobRightCard">
+                                        <Card.Title>Arbejdssted</Card.Title>
+                                        <Card.Text>Region: </Card.Text>
+                                        <Card.Text className="text-muted">1456 København K</Card.Text>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Container>
 
-                <Container className="detailed-Job-Description">
-                    <Container>
-                        <h2>Detailed job description</h2>
-                        <Row>
-                            <Col sm={8}>
-                                <Card>
-                                    <Card.Body>
-                                        <table className="table table-borderless">
-                                            <tbody>
-                                            <tr>
-                                                <th scope="row">Job start og slut</th>
-                                                <td>Starter: Snarest muligt
-                                                    <br></br>Stopper: 30. sep 2021
-                                                </td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Omfang</th>
-                                                <td>Konsulentopgave på fuld tid</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Erfaringsniveau</th>
-                                                <td>Junior<br></br>Senior</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Antal freelancere</th>
-                                                <td>2</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Afregning</th>
-                                                <td>Mark</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Opslagets friskhed</th>
-                                                <td>Spritny</td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Forventet svartid</th>
-                                                <td>Vi svarer løbende<br></br>
-                                                    <p className="text-muted">Virksomhedens seneste besked sendt 1 uge
-                                                        siden</p></td>
-                                            </tr>
-                                            </tbody>
-                                        </table>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                            <Col sm={4}>
-                                <Card>
-                                    <Card.Body>
-                                        <Card.Title>Company name</Card.Title>
-                                        <Card.Subtitle className="mb-2 text-muted">Company address</Card.Subtitle>
-                                        <Card.Text>Some quick example text to build on the card title and make
-                                            up the bulk of the card's content.</Card.Text>
-                                        <a href="#" className="card-link">Card link</a>
-                                        <a href="#" className="card-link">Another link</a>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>
+            </Container>
 
+            <Container className="detailed-Job-Description">
+                <Container>
+                    <h2>Detailed job description</h2>
+                    <Row>
+                        <Col sm={8}>
+                            <Card>
+                                <Card.Body>
+                                    <table className="table table-borderless">
+                                        <tbody>
+                                        <tr>
+                                            <th scope="row">Job start og slut</th>
+                                            <td>Starter: {this.state.job.jobStart}
+                                                <br></br>Stopper: {this.state.job.jobEnd}
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Deadline</th>
+                                            <td>{this.state.job.deadline}</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Erfaringsniveau</th>
+                                            <td>{this.state.job.experience}</td>
+                                        </tr>
+                                        <tr>
+                                            <th scope="row">Antal freelancere</th>
+                                            <td>{this.state.job.freelancers}</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col sm={4}>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>{this.state.job.companyName}</Card.Title>
+                                    <Card.Subtitle className="mb-2 text-muted">Company address</Card.Subtitle>
+                                    <Card.Text>Some quick example text to build on the card title and make
+                                        up the bulk of the card's content.</Card.Text>
+                                    <a href="#" className="card-link">Card link</a>
+                                    <a href="#" className="card-link">Another link</a>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Container>
 
-            </div>
-        )
+            </Container>
+        </div>;
     }
 
 

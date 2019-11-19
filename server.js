b66ab051696ab68c9778a9bf1c1babdd549f4028
Oliver <oliverskoeppen@gmail.com>
2019-11-19 16:20:46 +0100

    edit for heroku build

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/'));
-app.listen(process.env.PORT || 8080);
+const express = require('express');
+const http = require('http');
+const path = require('path');
+let app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT || '8080';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));

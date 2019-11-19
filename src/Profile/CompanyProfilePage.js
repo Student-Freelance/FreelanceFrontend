8e8c1eccad14291d294027079514526c0d6163dd
Frederik hjorth <s175109@student.dtu.dk>
2019-11-19 21:56:06 +0100

    navn, nummer og cvr bliver vist

--- src/Profile/CompanyProfilePage.js
+++ src/Profile/CompanyProfilePage.js
@@ -4,13 +4,22 @@ import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
 import ProfilePicture from "../Assets/profilepic.png";
 import ProfileCard from "../Card/ProfileCard";
+import {string} from "prop-types";
 
 
 class CompanyProfilePage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+
+
+    }
+
     render() {
         let company = JSON.parse(sessionStorage.getItem('User'));
-        let name = company.name;
         return(
             <div>
                 <Container>
@@ -24,8 +33,14 @@ class CompanyProfilePage extends Component {
                                     </Col>
                                     <Col xl={6} sm={6} md={6} xs={6}>
                                         <br/>
-                                        <h6>Name: {name}</h6>
+                                        <h6>Name: {company.companyName}</h6>
                                         <h6>Email: {company.email}</h6>
+                                    </Col>
+                                    <Col>
+                                        <h6>Vat: {company.vat} </h6>
+                                        <h6>Website: {company.website || 'no website'} </h6>
+                                        <h6>Phone Number: {company.phoneNumber}</h6>
+                                        <h6>Address: </h6>
                                     </Col>
                                 </Row>
                             </Card>

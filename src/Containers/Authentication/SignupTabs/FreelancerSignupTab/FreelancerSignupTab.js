23e250e25c5b3f5b56082e667b3c404aba40cfeb
UltimateDadBod <emilvinkel@gmail.com>
2019-11-25 14:46:15 +0100

    Signups are now using Store, and converted to functions

--- src/Containers/Authentication/SignupTabs/FreelancerSignupTab/FreelancerSignupTab.js
+++ src/Containers/Authentication/SignupTabs/FreelancerSignupTab/FreelancerSignupTab.js
@@ -1,105 +1,91 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import './FreelancerSignupTab.css';
 import {Button, Col, Form} from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
-import {AxiosAgent} from "../../../../Web/AxiosAgent";
-import {withAuth} from "react-auth-guard";
+import {observer} from "mobx-react";
+import {withRouter} from "react-router-dom";
+import {useStores} from "../../../../index";
 
-class FreelancerSignupTab extends Component {
-    state = {
+
+const FreelancerSignupTab = (props) => {
+    const {userStore} = useStores();
+    const [form, setState] = useState({
         email: "",
         userName: "",
         password: "",
         confirmPassword: "",
         firstName: "",
         lastName: ""
-
+    });
+    const handleChange = event => {
+        setState({
+            ...form,
+            [event.target.name]: event.target.value
+        });
     };
-
-    constructor(props) {
-        super(props);
-        this.auth = props.auth;
-    }
-
-    handleChange = e => {
-        this.setState({[e.target.name]: e.target.value})
-    };
-    handleSubmit = (e) => {
-        const body = this.state;
+    const handleSubmit = e => {
         e.preventDefault();
-        AxiosAgent.Post("Students",
-            {
-                ...body
-            }).then(result => {
-            this.auth.updateToken(result.data.token)
-        }).catch(error => {
-                console.log(error)
-            }
-        )
+        userStore.registerStudent({...form}).finally(() => {
+            userStore.loadingUser = false;
+            props.history.push("/")
+        })
     };
+    return (
+        <div>
+            <Container className="LoginForm">
+                <Form>
+                    <Form.Group>
+                        <Form.Row>
+                            <Col>
+                                <Form.Label>First name</Form.Label>
+                                <Form.Control type="text" value={form.firstName} name="firstName"
+                                              placeholder="Enter first name" onChange={handleChange}/>
+                            </Col>
+                            <Col>
+                                <Form.Label>Last name</Form.Label>
+                                <Form.Control type="text" value={form.lastName} name="lastName"
+                                              placeholder="Enter last name"
+                                              onChange={handleChange}/>
+                            </Col>
+                        </Form.Row>
+                    </Form.Group>
 
-    render() {
-        const {
-            email, userName, password, confirmPassword, firstName, lastName
-        }
-            = this.state;
-        return (
-            <div>
-                <Container className="LoginForm">
-                    <Form>
-                        <Form.Group>
-                            <Form.Row>
-                                <Col>
-                                    <Form.Label>First name</Form.Label>
-                                    <Form.Control type="text" value={firstName} name="firstName"
-                                                  placeholder="Enter first name" onChange={this.handleChange}/>
-                                </Col>
-                                <Col>
-                                    <Form.Label>Last name</Form.Label>
-                                    <Form.Control type="text" value={lastName} name="lastName"
-                                                  placeholder="Enter last name"
-                                                  onChange={this.handleChange}/>
-                                </Col>
-                            </Form.Row>
-                        </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" value={form.email} name="email" placeholder="Enter email"
+                                      onChange={handleChange}/>
+                    </Form.Group>
 
-                        <Form.Group controlId="formBasicEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" value={email} name="email" placeholder="Enter email"
-                                          onChange={this.handleChange}/>
-                        </Form.Group>
+                    <Form.Group controlId="formBasicUsername">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control type="text" value={form.userName} name="userName" placeholder="Enter username"
+                                      onChange={handleChange}/>
+                    </Form.Group>
 
-                        <Form.Group controlId="formBasicUsername">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" value={userName} name="userName" placeholder="Enter username"
-                                          onChange={this.handleChange}/>
-                        </Form.Group>
+                    <Form.Group>
+                        <Form.Row>
+                            <Col>
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" value={form.password} name="password"
+                                              placeholder="Enter password" onChange={handleChange}/>
+                            </Col>
+                            <Col>
+                                <Form.Label>Confirm password</Form.Label>
+                                <Form.Control type="password" value={form.confirmPassword} name="confirmPassword"
+                                              placeholder="Re-enter password" onChange={handleChange}/>
+                            </Col>
+                        </Form.Row>
+                    </Form.Group>
 
-                        <Form.Group>
-                            <Form.Row>
-                                <Col>
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" value={password} name="password"
-                                                  placeholder="Enter password" onChange={this.handleChange}/>
-                                </Col>
-                                <Col>
-                                    <Form.Label>Confirm password</Form.Label>
-                                    <Form.Control type="password" value={confirmPassword} name="confirmPassword"
-                                                  placeholder="Re-enter password" onChange={this.handleChange}/>
-                                </Col>
-                            </Form.Row>
-                        </Form.Group>
+                    <Button
+                        onClick={(e) => handleSubmit(e)}
+                        variant="primary" type="submit" size="lg" block>
+                        Sign up
+                    </Button>
+                </Form>
+            </Container>
+        </div>
+    );
+};
 
-                        <Button
-                            onClick={(e) => this.handleSubmit(e)}
-                            variant="primary" type="submit" size="lg" block>
-                            Sign up
-                        </Button>
-                    </Form>
-                </Container>
-            </div>
-        );
-    }
-}
-
-export default withAuth(FreelancerSignupTab);
+export default withRouter(observer(FreelancerSignupTab));
--- src/Web/ApiAgent.js
+++ src/Web/ApiAgent.js
@@ -4,7 +4,7 @@ import {toast} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 const superagent = superagentPromise(_superagent, global.Promise);
-const API_ROOT = 'https://devops01.eitlab.diplom.dtu.dk/api/';//'https://localhost:5001/api/';
+const API_ROOT = 'https://devops01.eitlab.diplom.dtu.dk/api/'; //'https://localhost:5001/api/'; 
 
 const handleErrors = (err) => {
     console.log(err);
@@ -12,15 +12,33 @@ const handleErrors = (err) => {
     if(string.includes('Request has been terminated')){
         toast.error("Der er problemer med at forbinde til serveren");
         return;
-    } else if (err.response.status === 401 || err.response.status === 400) {
+    }
+    if (err.response.status === 400) {
+        console.log(err.response.body.errors);
+        toast.error("Formen er ikke korrekt udfyldt");
+        return;
+    }
+    if (err.response.status === 401) {
         console.log(err.response.text);
         toast.error("Brugernavn eller kodeord er forkert!");
-        return err.response.text;
+        return;
+    }
+    if (err.response.status === 500) {
+        console.log(err.response.text);
+        toast.error("Der skete en serverfejl, vi arbejder på sagen.");
+        return;
+    }
+    if (err.response.status === 404) {
+        console.log(err.response.text);
+        toast.error("Brugeren blev ikke fundet");
+        return;
     }
    return err;
 };
 
-const responseBody = res => res.body;
+const responseBody = res => {
+    if (!(res === undefined)) return res.body;
+};
 
 const tokenPlugin = req => {
     let token = localStorage.getItem("Token");

c5e6af4189bf228313b188f421faf623c659d80e
Frederik hjorth <s175109@student.dtu.dk>
2019-11-29 09:25:32 +0100

    nu sker der noget

--- src/Profile/StudentProfilePage.js
+++ src/Profile/StudentProfilePage.js
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import "./StudentProfilePage.css"
 import Image from "react-bootstrap/Image";
 import ProfilePicture from "../Assets/profilepic.png"
-import {Col, InputGroup, Row} from "react-bootstrap";
+import {Col, InputGroup, Row, Form} from "react-bootstrap";
 import ProfileCard from "../Components/Card/ProfileCard";
 import {observer} from "mobx-react";
 import {useStores} from "../index";
@@ -16,7 +16,7 @@ const StudentProfilePage = (props) => {
 
     const {userStore} = useStores();
     const [student, setStudent] = useState({
-        email: "",
+        //email: "",
         phoneNumber: "",
         semester: 0,
         resume: "",
@@ -26,18 +26,110 @@ const StudentProfilePage = (props) => {
         isStudent: true
     });
 
+    const [edit, setEdit] = useState({
+        editMode: false,
+        btnText: "edit"
+    });
+
     const handleIndputChange = event => {
         const {name, value} = event.target;
         setStudent({...student, [name]: value})
     };
 
     const handleSubmit = event => {
-        event.preventDefault();
-        userStore.updateUser({...student});
+        setEdit( {editMode: !edit.editMode});
+        console.log(edit.editMode);
+        // edit.editMode ? setEdit({btnText: "edit"}) : setEdit({btnText: "save"});
+        if(edit.editMode){
+            console.log(student);
+            event.preventDefault();
+            userStore.updateUser({...student});
+        }
     };
 
         return (
             <div>
+                {edit.editMode ?
+
+                <Container>
+                    <h3 className="profilHeadline">Profile</h3>
+                    <Row sm={12} md={12} xl={12}>
+                        <InputGroup>
+                            <Col xl={5}>
+                                <Card body>
+                                    <Row sm={12} md={12} xl={12}>
+                                        <Col xl={5} sm={5} md={5} xs={5}>
+                                            <Image fluid
+                                                   src={((userStore.studentUser.logo) === '' || null || 'string') ? ProfilePicture : userStore.studentUser.logo}
+                                                   alt={"No image found"} roundedCircle/>
+                                        </Col>
+                                        <Col xl={7} sm={7} md={7} xs={7}>
+                                            <br/>
+                                            <h6>Name: {userStore.studentUser.firstname + " " + userStore.studentUser.lastname} </h6>
+                                            <h6>Email: {userStore.studentUser.email}</h6>
+                                        </Col>
+                                    </Row>
+                                </Card>
+                                <br/>
+                                <Card body>
+                                    <Row sm={12} md={12} xl={12}>
+                                        <Col xl={7}>
+                                            <label>Telefon nummer:</label>
+                                            <input
+                                                type="number" maxLength={8} value={student.phoneNumber} name="phoneNumber"
+                                                placeholder={userStore.studentUser.phoneNumber}
+                                                onChange={handleIndputChange}
+                                            />
+                                            <h6>Tag: {userStore.studentUser.tags}</h6>
+                                            <h6>Semester: {userStore.studentUser.semester}</h6>
+                                        </Col>
+                                    </Row>
+                                </Card>
+                                <br/>
+                            </Col>
+                            <Col xl={7}>
+                                <Card body>
+                                    <Row sm={12} md={12} xl={12}>
+                                        <Col xl={10} md={10} sm={10} xs={10}>
+                                            <h6 className="resume"> Resume</h6>
+                                        </Col>
+                                        <hr/>
+                                        <p>{userStore.studentUser.resume}</p>
+                                        <Form.Group controlId="exampleForm.ControlTextarea1">
+                                            <Form.Label>Example textarea</Form.Label>
+                                            <Form.Control as="textarea" rows="3" />
+                                        </Form.Group>
+                                    </Row>
+                                    <hr/>
+                                </Card>
+                                <br/>
+                                <Card body>
+                                    <h6 className="education">Min uddannelse</h6>
+                                    <hr/>
+                                    <p>{userStore.studentUser.education}</p>
+                                </Card>
+                                <br/>
+                                <Card body>
+                                    <h6 className="experience">Min erfaring</h6>
+                                    <hr/>
+                                    <p>{userStore.studentUser.experience}</p>
+                                </Card>
+                                <br/>
+                                <Card body>
+                                    <h6 className="skills">Mine kompetencer</h6>
+                                    <hr/>
+                                    <p>{userStore.studentUser.competences}</p>
+                                </Card>
+                                <Button
+                                    onClick={(event) => handleSubmit(event)}
+                                    variant="primary" type="submit" size="lg" block>
+                                    {edit.btnText}
+                                </Button>
+                            </Col>
+                        </InputGroup>
+                    </Row>
+                </Container>
+                :
                 <Container>
                     <h3 className="profilHeadline">Profile</h3>
                     <Row sm={12} md={12} xl={12}>
@@ -61,18 +153,7 @@ const StudentProfilePage = (props) => {
                             <Card body>
                                 <Row sm={12} md={12} xl={12}>
                                     <Col xl={7}>
-                                        <label>Telefon nummer:</label>
-                                        <input
-                                            type="number" maxLength={8} value={student.phoneNumber} name="phoneNumber"
-                                            placeholder={userStore.studentUser.phoneNumber}
-                                            onChange={handleIndputChange}
-                                        />
-                                        <Button
-                                            onClick={(event) => handleSubmit(event)}
-                                            variant="primary" type="submit" size="lg" block>
-                                            Edit
-                                        </Button>
-                                        <h6>Telefon nummer: {student.email}</h6>
+                                        <label>Telefon nummer: {userStore.studentUser.phoneNumber}</label>
                                         <h6>Tag: {userStore.studentUser.tags}</h6>
                                         <h6>Semester: {userStore.studentUser.semester}</h6>
                                     </Col>
@@ -109,11 +190,15 @@ const StudentProfilePage = (props) => {
                             <hr/>
                             <p>{userStore.studentUser.competences}</p>
                         </Card>
-
+                            <Button
+                                onClick={(event) => handleSubmit(event)}
+                                variant="primary" type="submit" size="lg" block>
+                                {edit.btnText}
+                            </Button>
                         </Col>
                         </InputGroup>
                     </Row>
-                </Container>
+                </Container>}
             </div>
         )
 };
